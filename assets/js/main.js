e15cf7dcb48b9a2160b20c471c36fd4c57a1cae1
Anh Siêu Nhân <hoangthuathanhan@gmail.com>
2021-10-16 19:38:31 +0700

    them form search

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
 ;(function($){
     "use strict"
 
+    function showSearchBox() {
+        $('.block-search a').on('click', function(e) {            
+            e.preventDefault();
+            $('.block-default-search').addClass('active');
+        });
+
+        $('.block-default-search .close').on('click', function() {
+            $('.block-default-search').removeClass('active');
+        });
+    }
 
     // DOM Read
     $(function() {
-        
+        showSearchBox();
     });
 
 })(jQuery);
